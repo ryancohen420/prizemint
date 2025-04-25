216d2f2f622faea19f78c27f6349bd384ba4291a
samuel <137750141+ryancohen420@users.noreply.github.com>
2025-04-25 18:10:16 +1000

    nextauth v4

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import NextAuth from "next-auth";
+// src/app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth/next";
 import { authOptions } from "@/lib/session";
 
 const handler = NextAuth(authOptions);
